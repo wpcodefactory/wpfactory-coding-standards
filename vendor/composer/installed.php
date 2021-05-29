68b5de276cdc4ea6759dc47ec6fe87c089d48c1a
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-05-29 18:19:58 -0300

    first commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,78 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'dealerdirect/phpcodesniffer-composer-installer' => 
+    array (
+      'pretty_version' => 'v0.7.1',
+      'version' => '0.7.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fe390591e0241955f22eb9ba327d137e501c771c',
+    ),
+    'phpcompatibility/php-compatibility' => 
+    array (
+      'pretty_version' => '9.3.5',
+      'version' => '9.3.5.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9fb324479acf6f39452e0655d2429cc0d3914243',
+    ),
+    'phpcompatibility/phpcompatibility-paragonie' => 
+    array (
+      'pretty_version' => '1.3.1',
+      'version' => '1.3.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ddabec839cc003651f2ce695c938686d1086cf43',
+    ),
+    'phpcompatibility/phpcompatibility-wp' => 
+    array (
+      'pretty_version' => '2.1.1',
+      'version' => '2.1.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b7dc0cd7a8f767ccac5e7637550ea1c50a67b09e',
+    ),
+    'squizlabs/php_codesniffer' => 
+    array (
+      'pretty_version' => '3.6.0',
+      'version' => '3.6.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'ffced0d2c8fa8e6cdc4d695a743271fab6c38625',
+    ),
+    'wp-coding-standards/wpcs' => 
+    array (
+      'pretty_version' => '2.3.0',
+      'version' => '2.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7da1894633f168fe244afc6de00d141f27517b62',
+    ),
+  ),
+);
---
+++ vscode-phpcs.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Asd.
+ *
+ * @package test
+ * @version
+ * @since
+ */
+
+$test = array(
+	'a' => 'asd',
+	'b' => 'asdad',
+	'c' => 'sd',
+	'd' => 'ssss d',
+	'e' => array( 'a', 'b', 12 ),
+);
+
+if ( true === 'a' ) {
+	echo 'sd';
+}
