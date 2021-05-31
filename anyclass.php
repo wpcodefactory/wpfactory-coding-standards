3a9fbc8dd78a2a50fbe2cda0cb2390074fca0a4d
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-05-31 12:00:05 -0300

    Update gitignore

---
+++ anyclass.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * WPF Promoting Notice - Core.
+ *
+ * @version 1.0.0
+ * @since   1.0.0
+ * @author  WPFactory
+ */
+
+namespace WPFactory\Promoting_Notice;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+if ( ! class_exists( 'AnyClass' ) ) {
+	class AnyClass {
+		private $args = [];
+	}
+}
