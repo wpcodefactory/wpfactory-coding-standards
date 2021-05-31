da16d29edc12ff54665d6cfcce415c2d0e135ea5
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-05-31 00:45:33 -0300

    Update file

--- coding-standards-tester.php
+++ coding-standards-tester.php
@@ -1,4 +1,8 @@
 <?php
 /**
  * Test any WordPress coding standards you want here.
+ *
+ * If your IDE/Code editor is configured just right, it should warn you about any coding standards issues.
+ * You should be able to fix most of them automatically by simply saving the file.
+ *
  * /
