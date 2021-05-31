800c41bbfc9dc036cade69514e795abb73cca5ed
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2021-05-31 00:53:59 -0300

    Update comment

--- coding-standards-tester.php
+++ coding-standards-tester.php
@@ -4,5 +4,4 @@
  *
  * If your IDE/Code editor is configured just right, it should warn you about any coding standards issues.
  * You should be able to fix most of them automatically by simply saving the file.
- *
  * /
